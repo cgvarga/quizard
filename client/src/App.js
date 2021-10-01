7fcaac28ade6c65700b208c540f45e462ca36c4e
JTsang95 <JTsang95@users.noreply.github.com>
2021-10-01 14:21:41 -0400

    Added some styling for login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,37 @@
 import React from 'react';
-import { Form, Button } from 'react-bootstrap';
+import './bootstrap.min.css';
+import { Form, Button, Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 const App = () => {
 	return (
 		<>
-			<h1>Login</h1>
-			<Form>
-				<Form.Group controlId='email'>
-					<Form.Label>Email Address</Form.Label>
-					<Form.Control type='email' placeholder='Enter email'></Form.Control>
-				</Form.Group>
+			<Header />
+			<Container>
+				{/* TODO refactor login page */}
+				<h1>Login</h1>
+				<Form>
+					<Form.Group controlId='email'>
+						<Form.Label>Email Address</Form.Label>
+						<Form.Control type='email' placeholder='Enter email'></Form.Control>
+					</Form.Group>
 
-				<Form.Group controlId='password'>
-					<Form.Label>Password</Form.Label>
-					<Form.Control
-						type='password'
-						placeholder='Enter password'
-					></Form.Control>
-				</Form.Group>
+					<Form.Group controlId='password'>
+						<Form.Label>Password</Form.Label>
+						<Form.Control
+							type='password'
+							placeholder='Enter password'
+						></Form.Control>
+					</Form.Group>
 
-				<Button type='submit' variant='primary'>
-					Sign In
-				</Button>
-			</Form>
+					<Button type='submit' variant='primary'>
+						Sign In
+					</Button>
+				</Form>
+			</Container>
+
+			<Footer />
 		</>
 	);
 };
