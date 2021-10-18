e60a303e27e3919b35e7cea5dfa65e7949632775
JTsang95 <JTsang95@users.noreply.github.com>
2021-10-18 11:54:57 -0400

    Implemented quiz taking & page navigation

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,14 +1,61 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { Button, Container } from 'react-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Quiz = () => {
-	// const { questions } = quiz;
+	const { questions } = useSelector((state) => state.quizLoader.quiz);
+
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+
+	const answerButtonHandler = (isCorrect) => {
+		if (isCorrect === true) {
+			setScore(score + 1);
+		}
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+
+	const resetQuizHandler = () => {
+		setCurrentQuestion(0);
+		setShowScore(false);
+		setScore(0);
+	};
+
 	return (
-		<div>
-			{/* {questions.map((question) => (
-				<p>{questions.value}</p>
-			))} */}
-			<p>Sample Quiz</p>
-		</div>
+		<Container>
+			{showScore ? (
+				<div className='score-section'>
+					You scored {score} out of {questions.length}
+					<div>
+						<Button onClick={resetQuizHandler}>Retake Quiz</Button>
+					</div>
+				</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>
+							{questions[currentQuestion].value}
+						</div>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestion].answers.map((answerOption) => (
+							<Button onClick={() => answerButtonHandler(answerOption.correct)}>
+								{answerOption.value}
+							</Button>
+						))}
+					</div>
+				</>
+			)}
+		</Container>
 	);
 };
 
--- client/src/components/QuizList.js
+++ client/src/components/QuizList.js
@@ -1,40 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Card, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { load, reset } from '../redux/quizLoader';
 import QuizService from '../services/QuizService';
 
-export default class QuizList extends React.Component {
-	constructor() {
-		super();
+const QuizList = () => {
+	const [quizes, setQuizes] = useState([]);
+	const dispatch = useDispatch();
 
-		this.state = {
-			quizes: [],
-		};
-	}
-
-	componentDidMount() {
+	useEffect(() => {
 		QuizService.getQuizes().then((res) => {
-			this.setState({ quizes: res.data });
+			setQuizes(res.data);
 		});
-	}
+	}, [dispatch]);
 
-	render() {
-		return (
-			<>
-				<Container>
-					<h1>Quizes</h1>
-					{this.state.quizes.map((quiz) => (
-						<Card key={quiz.title}>
-							<Card.Body>
-								<Card.Title>{quiz.title}</Card.Title>
-								<Link to={`/quiz/${quiz.title}`}>
-									<Button variant='primary'>Take Quiz</Button>
-								</Link>
-							</Card.Body>
-						</Card>
-					))}
-				</Container>
-			</>
-		);
-	}
-}
+	const takeQuizHandler = (quiz) => {
+		dispatch(load(quiz));
+	};
+
+	return (
+		<>
+			<Container>
+				<h1>Quizes</h1>
+				{quizes.map((quiz) => (
+					<Card key={quiz.title}>
+						<Card.Body>
+							<Card.Title>{quiz.title}</Card.Title>
+							<Link to={`/quiz/${quiz.title}`}>
+								<Button variant='primary' onClick={() => takeQuizHandler(quiz)}>
+									Take Quiz
+								</Button>
+							</Link>
+						</Card.Body>
+					</Card>
+				))}
+			</Container>
+		</>
+	);
+};
+
+export default QuizList;
